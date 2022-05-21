ed47f8a7f649cffaeb04af201a6d83ad3e9e5452
Damyan Slavov <dss2005@abv.bg>
2022-05-21 19:33:12 +0300

    fixed winforms app

--- App/Home.Designer.cs
+++ App/Home.Designer.cs
@@ -19,16 +19,10 @@ partial class Home
     {
             this.buttonStart = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.textBoxWidth = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.textBoxHeight = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -50,19 +44,10 @@ partial class Home
             this.label2.Size = new System.Drawing.Size(0, 45);
             this.label2.TabIndex = 4;
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.textBoxWidth);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(437, 50);
-            this.panel1.TabIndex = 5;
-            // 
             // textBoxWidth
             // 
-            this.textBoxWidth.Dock = System.Windows.Forms.DockStyle.Right;
-            this.textBoxWidth.Location = new System.Drawing.Point(267, 0);
+            this.textBoxWidth.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBoxWidth.Location = new System.Drawing.Point(282, 45);
             this.textBoxWidth.Name = "textBoxWidth";
             this.textBoxWidth.Size = new System.Drawing.Size(170, 50);
             this.textBoxWidth.TabIndex = 1;
@@ -70,42 +55,19 @@ partial class Home
             // 
             // label1
             // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(12, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(150, 50);
             this.label1.TabIndex = 0;
             this.label1.Text = "Width (ch)";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.flowLayoutPanel1.Controls.Add(this.panel1);
-            this.flowLayoutPanel1.Controls.Add(this.panel2);
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 47);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(443, 124);
-            this.flowLayoutPanel1.TabIndex = 6;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.textBoxHeight);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Location = new System.Drawing.Point(3, 71);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(437, 50);
-            this.panel2.TabIndex = 6;
-            // 
             // textBoxHeight
             // 
-            this.textBoxHeight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.textBoxHeight.Location = new System.Drawing.Point(267, 0);
+            this.textBoxHeight.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBoxHeight.Location = new System.Drawing.Point(282, 120);
             this.textBoxHeight.Name = "textBoxHeight";
             this.textBoxHeight.Size = new System.Drawing.Size(170, 50);
             this.textBoxHeight.TabIndex = 1;
@@ -113,9 +75,9 @@ partial class Home
             // 
             // label3
             // 
-            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Location = new System.Drawing.Point(12, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(158, 50);
             this.label3.TabIndex = 0;
@@ -125,17 +87,15 @@ partial class Home
             // Home
             // 
             this.ClientSize = new System.Drawing.Size(464, 472);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonStart);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Name = "Home";
             this.Text = "Home";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,11 +105,8 @@ partial class Home
 
     private Button buttonStart;
     private Label label2;
-    private Panel panel1;
     private TextBox textBoxWidth;
     private Label label1;
-    private FlowLayoutPanel flowLayoutPanel1;
-    private Panel panel2;
     private TextBox textBoxHeight;
     private Label label3;
 }
